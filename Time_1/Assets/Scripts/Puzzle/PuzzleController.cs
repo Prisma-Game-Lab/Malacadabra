70fa07e3dbf4948532e4a53957aa0557c5e92ed0
rafaelrc7 <rafaelrc7@gmail.com>
2022-05-20 17:19:19 -0300

    Implement puzzle

---
+++ Time_1/Assets/Scripts/Puzzle/PuzzleController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleController : MonoBehaviour
+{
+	[SerializeField] int[] code = {2, 1, 3};
+	[SerializeField] GameObject[] buttons;
+	[SerializeField] GameObject prize;
+
+	private int[] attempt;
+	private int tries = 0;
+
+	private void Awake() {
+		attempt = new int[code.Length];
+		Restart();
+	}
+
+	private void Restart() {
+		foreach (var button in buttons)
+			button.SetActive(true);
+		prize.SetActive(false);
+		tries = 0;
+	}
+
+	private void Win() {
+		foreach (var button in buttons)
+			button.SetActive(false);
+		prize.SetActive(true);
+	}
+
+	private bool isCodeRight() {
+		for (int i = 0; i < attempt.Length; ++i) {
+			if (attempt[i] != code[i])
+				return false;
+		}
+
+		return true;
+	}
+
+	public void Try(int code) {
+		attempt[tries++] = code;
+		buttons[code-1].SetActive(false);
+
+		if (tries == attempt.Length) {
+			if (isCodeRight())
+				Win();
+			else
+				Restart();
+		}
+	}
+}
+
