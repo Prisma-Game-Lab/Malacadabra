dea6b18dd16d73bb5755880b3eb78525a4b12e95
rafaelrc7 <rafaelrc7@gmail.com>
2022-06-10 17:26:59 -0300

    Disable dragging when page is in place

--- Time_1/Assets/Scripts/DragAndDrop/GameMec.cs
+++ Time_1/Assets/Scripts/DragAndDrop/GameMec.cs
@@ -5,40 +5,46 @@ using UnityEngine.EventSystems;
 
 public class GameMec : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    [SerializeField] private RectTransform _transform;
-    [SerializeField] private RectTransform _transform2;
-    [SerializeField] private CanvasGroup _canvasGroup;
-    [SerializeField] private PuzzleManager _puzzleManager;
-	[SerializeField] private int pageTreshold = 5;
+	[SerializeField] private RectTransform _transform;
+	[SerializeField] private RectTransform _transform2;
+	[SerializeField] private CanvasGroup _canvasGroup;
+	[SerializeField] private PuzzleManager _puzzleManager;
+	[SerializeField] private int _pageTreshold = 7;
+	[SerializeField] private bool _inplace = false;
 
 
-    public void OnBeginDrag(PointerEventData eventData)
-    {
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		if (_inplace) return;
+		_canvasGroup.blocksRaycasts = false;
+	}
 
-        _canvasGroup.blocksRaycasts = false;
-    }
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		if (_inplace) return;
+		_canvasGroup.blocksRaycasts = true;
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        _canvasGroup.blocksRaycasts = true;
-
-        if (Vector2.Distance(_transform.anchoredPosition, _transform2.anchoredPosition) < pageTreshold)
-        {
+		if (Vector2.Distance(_transform.anchoredPosition, _transform2.anchoredPosition) < _pageTreshold)
+		{
+			_inplace = true;
 			_transform.anchoredPosition = _transform2.anchoredPosition;
-            Debug.Log("Soltou");
-            _puzzleManager.somaPag();
-        }
-    }
+			Debug.Log("Soltou");
+			_puzzleManager.somaPag();
+		}
+	}
 
-    public void OnDrag(PointerEventData eventData)
-    {
-        _transform.anchoredPosition += eventData.delta;
+	public void OnDrag(PointerEventData eventData)
+	{
+		if (_inplace) return;
+		_transform.anchoredPosition += eventData.delta;
 
-    }
+	}
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        Debug.Log("Apertou");
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		if (_inplace) return;
+		Debug.Log("Apertou");
 
-    }
+	}
 }
+
