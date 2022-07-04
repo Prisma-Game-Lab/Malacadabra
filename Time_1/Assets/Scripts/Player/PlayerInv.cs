5dea49963f9ea9f5c4c48f951f533dd9012726da
FelipeVassalo <105084783+FelipeVassalo@users.noreply.github.com>
2022-07-04 14:37:53 -0300

    Estou commitando de novo

--- Time_1/Assets/Scripts/Player/PlayerInv.cs
+++ Time_1/Assets/Scripts/Player/PlayerInv.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerInv : MonoBehaviour
 {
 	private bool hasPage = false;
+	private bool hasPage2 = false;
 	private bool hasRing = false;
 	private bool hasScrewdriver = false;
 	[SerializeField] private GameObject ringIndicator;
 	[SerializeField] private GameObject pageIndicator;
 	[SerializeField] private GameObject screwdriverIndicator;
+	[SerializeField] private GameObject pageIndicator2;
 
 	public bool HasScrewdriver
 	{
@@ -42,11 +44,23 @@ public class PlayerInv : MonoBehaviour
 			pageIndicator.SetActive(hasPage);
 		}
 	}
+	public bool HasPage2
+	{
+		get => hasPage2;
+		set
+		{
+			hasPage2 = value;
+
+			pageIndicator.SetActive(hasPage2);
+		}
+	}
 	private void Awake() {
 		screwdriverIndicator.SetActive(hasScrewdriver);
 		ringIndicator.SetActive(hasRing);
 		pageIndicator.SetActive(hasPage);
+		pageIndicator2.SetActive(hasPage2);
 
-	}
+
+}
 }
 
