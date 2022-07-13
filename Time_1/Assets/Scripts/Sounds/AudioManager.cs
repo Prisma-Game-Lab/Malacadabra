aed513133a06e06260b17ee0b5b6013a3ef8f922
Apenasumcara <vscamerano@gmail.com>
2022-07-13 18:25:59 -0300

    Adiciona novo AudioManager e menu de config

--- Time_1/Assets/Scripts/Sounds/AudioManager.cs
+++ Time_1/Assets/Scripts/Sounds/AudioManager.cs
@@ -1,79 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Audio;
 using System;
 using UnityEngine;
-using System.Diagnostics;
-using UnityEngine.Audio;
-using Debug = UnityEngine.Debug;
 
 public class AudioManager : MonoBehaviour
 {
-    public Sound[] sounds;
-    public static AudioManager instance;
-    private void Awake()
-    {
-        if (instance == null)
-            instance = this;
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
-        DontDestroyOnLoad(gameObject);
 
-        if (PlayerPrefs.GetInt("FirstGame") == 0)
-        {
-            PlayerPrefs.SetFloat("MasterSoundPrefs", 100f);
-            PlayerPrefs.SetInt("FirstGame", 1);
-        }
+	//public AudioMixerGroup mixerGroup;
 
-        foreach (Sound s in sounds)
-        {
-            s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
-            s.source.volume = PlayerPrefs.GetFloat("MasterSoundPrefs") * 0.01f;
-            s.source.pitch = s.pitch;
-            s.source.loop = s.loop;
-        }
-        UpdateSoundVolumes();
-    }
+	public Sound[] sounds;
+	public static AudioManager instance;
 
-    private void Start()
-    {
-        
-    }
-    public void Play(string name)
-    {
-        
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s != null)
-        {
-            s.source.Play();
-            return;
-        }
-        s.source.Play();
-    }
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad(gameObject);
 
-    public void Stop(string name)
-    {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s != null)
-        {
-            s.source.Stop();
-            return;
-        }
-        s.source.Stop();
-    }
-    public void StopAllSounds()
-    {
-        foreach (Sound sound in sounds)
-        {
-            sound.source.Stop();
-        }
-    }
-    public void UpdateSoundVolumes()
-    {
-        foreach (Sound s in sounds)
-            s.source.volume = PlayerPrefs.GetFloat("MasterSoundPrefs");
-    }
+		foreach (Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			s.source.loop = s.loop;
+
+			s.source.outputAudioMixerGroup = s.mixerGroup;
+		}
+	}
+
+	public void Play(string sound)
+	{
+		Sound s = Array.Find(sounds, item => item.name == sound);
+		if (s == null)
+		{
+			Debug.LogWarning("Sound: " + sound + " not found!");
+			return;
+		}
+
+		s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volumeVariance / 2f, s.volumeVariance / 2f));
+		s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitchVariance / 2f, s.pitchVariance / 2f));
+
+		s.source.Play();
+	}
+
 }
--- Time_1/Assets/Scripts/Sounds/Sound.cs
+++ Time_1/Assets/Scripts/Sounds/Sound.cs
@@ -1,21 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.Audio;
 using UnityEngine;
-using UnityEngine.Audio;
 
 [System.Serializable]
-public class Sound
-{
-    public string name;
-    public AudioClip clip;
+public class Sound {
+	public string name;
 
-    [Range(0f, 1f)]
-    public float volume;
+	public AudioClip clip;
 
-    [Range(.1f, 3f)]
-    public float pitch;
+	[Range(0f, 1f)]
+	public float volume = .75f;
+	[Range(0f, 1f)]
+	public float volumeVariance = .1f;
 
-    public bool loop;
+	[Range(.1f, 3f)]
+	public float pitch = 1f;
+	[Range(0f, 1f)]
+	public float pitchVariance = .1f;
 
-    [HideInInspector] public AudioSource source;
+	public bool loop = false;
+
+	public AudioMixerGroup mixerGroup;
+
+	[HideInInspector]
+	public AudioSource source;
+
 }
