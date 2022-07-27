d5be3ceadc304ffec702e223a18b1eedf807be31
MadudaJellyfish <105326941+MadudaJellyfish@users.noreply.github.com>
2022-07-27 20:04:39 -0300

    mudanças do puzzle do relogio

--- Time_1/Assets/Scripts/Puzzle/ClockPosition.cs
+++ Time_1/Assets/Scripts/Puzzle/ClockPosition.cs
@@ -3,16 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ClockPosition : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{   
+    private GameObject hand = getClickedObject();
+    private int Small = 3;
+    private int Mid = 12;
+    private int Big = 9;
+    [serialiazedfield]
+    private GameObject circle;
+    public bool correctPosicion = false
+    private void OnMouseDown ()
     {
+        if(hand.tag == "ClockHandBig")
+        {
+            ClockHandBig.Rotate(Vector3.back, 2.5f);   
+        }
+        elseif(hand.tag == "ClockHandMid")
+        {
+            ClockHandMid.Rotate(Vector3.back, 2.5f);
+        }
+        else(hand.tag == "ClockHandSmall")
+        {
+            ClockHandSmall.Rotate(Vector3.back, 2.5f);
+        }
         
+
+    }
+        
+        private void CheckHands ()
+    {
+        if(ClockHandBig != Big) && (ClockHandMid != Mid) && (ClockHandSmall != Small)
+        {
+            correctPosicion = false
+
+        }
+    }
+    correctPosicion = true
+
+    
+    public Update ()
+    {
+        if(CheckHands == true)
+        {
+            circle.SetActive(true);
+        }
+    circle.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
