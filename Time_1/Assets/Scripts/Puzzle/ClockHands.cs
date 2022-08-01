15de7355b7cb47157054ea4ad04c76eb1f038b94
Apenasumcara <vscamerano@gmail.com>
2022-08-01 15:16:34 -0300

    Arruma Puzzle do relogio
    dps de mt sofrimento, agr foi

--- Time_1/Assets/Scripts/Puzzle/ClockHands.cs
+++ Time_1/Assets/Scripts/Puzzle/ClockHands.cs
@@ -13,53 +13,30 @@ public class ClockHands : MonoBehaviour
     public GameObject ClockHandMid;
     public GameObject ClockHandSmall;
     public GameObject circle;
-    
-    private void getClickedObject()
+    private bool completed = false;
+         
+    private bool CheckHands()
     {
-        if(hand.tag == "ClockHandBig")
+        if((Mathf.Abs(ClockHandBig.transform.rotation.eulerAngles.z - (360 - Big)) < error ) 
+        && (Mathf.Abs(ClockHandMid.transform.rotation.eulerAngles.z - (360 - Mid)) < error ) 
+        && (Mathf.Abs(ClockHandSmall.transform.rotation.eulerAngles.z - (360 - Small)) < error ))
         {
-            ClockHandBig.transform.Rotate(Vector3.back);   
-        }
-        else if(hand.tag == "ClockHandMid")
-        {
-            ClockHandMid.transform.Rotate(Vector3.back);
-        }
-        else
-        {
-            ClockHandSmall.transform.Rotate(Vector3.back);
-        }
-        
-
-    }
-        
-    private bool CheckHands ()
-    {
-        if(( Mathf.Abs(ClockHandBig.transform.rotation.eulerAngles.z - Big) < error ) 
-        && (Mathf.Abs(ClockHandMid.transform.rotation.eulerAngles.z - Mid) < error ) 
-        && (Mathf.Abs(ClockHandSmall.transform.rotation.eulerAngles.z - Small) < error ))
-        {
-            
             return true;
-
         }
     return false;
     }
     
-
-    
     void Update ()
     {
-        if(CheckHands() == true)
+        CheckHands();
+        //Debug.Log((ClockHandBig.transform.rotation.eulerAngles.z - (360 - Big)));
+        //Debug.Log("Angulo Euler" + ClockHandBig.transform.rotation.eulerAngles.z);
+        if(CheckHands() && !completed)
         {
             circle.SetActive(true);
+            completed = true;
             
         }
-    else
-    {
-        CheckHands();
-    }
-    }
-
-    
+    }    
 }
 
